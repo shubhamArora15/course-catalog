f802254f9792f0185e5e5524ab50780003d372fc
shubhamarora15 <ashubham.1994@gmail.com>
2020-06-23 00:44:57 +0530

    home service :: categories and courses business logic created and connected to component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [HomeService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private course: HomeService) {
+
+  }
 
   ngOnInit(): void {
+    // Get all categories 
+    this.getCategories();
+
+    // Get all courses
+    this.getCourses();
+  }
+
+  getCategories() {
+    this.course.getCategories().subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  getCourses() {
+    this.course.getCategories().subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { courses } from '../models/course.model'
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  private categories_url = "https://frontend-hiring.appspot.com/all_categories";
+  private courses_url = "https://frontend-hiring.appspot.com/all_courses";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getCategories() {
+    return this.http.get(this.categories_url);
+  }
 }
