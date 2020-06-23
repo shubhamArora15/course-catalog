d836a011922d574fa81f7cad7b7ef18c87dfe852
shubhamarora15 <ashubham.1994@gmail.com>
2020-06-23 14:31:41 +0530

    filter improved

--- src/app/filters/courses.pipe.ts
+++ src/app/filters/courses.pipe.ts
@@ -3,25 +3,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 // To handle different filter
 @Pipe({ name: 'coursesFilter' })
 export class CoursesFilter implements PipeTransform {
-    transform(categories, keyword: string, category: string) {
+    transform(courses, keyword: string, category: string) {
 
-        if (!keyword && (!category ||  category.trim().toLowerCase() === 'all')) {
-            return categories
+        if (!keyword && (!category || category.trim().toLowerCase() === 'all')) {
+            return courses
         }
-        let keyWordFilters = categories.filter(item => { return item.title.trim().toLowerCase().includes(keyword.trim().toLowerCase()) });
-        let categoryFilters = categories.filter(item => { return item.category.trim().toLowerCase().includes(category.trim().toLowerCase()) })
-
         if (keyword && !category || (keyword && category.trim().toLowerCase() === 'all')) {
-            return keyWordFilters
+            return courses.filter(item => {
+                return item.title.trim().toLowerCase().includes(keyword.trim().toLowerCase()) ||
+                    item.instructor.trim().toLowerCase().includes(keyword.trim().toLowerCase())
+            });
         }
 
         if (!keyword && category) {
-            return categoryFilters
+            return courses.filter(item => {
+                return item.category.trim().toLowerCase().includes(category.trim().toLowerCase())
+            })
+
         }
 
         if (keyword && category) {
-            return keyWordFilters
-                .filter(item => { return item.category.trim().toLowerCase().includes(category.trim().toLowerCase()) })
+            return courses.filter(item => {
+                return item.title.trim().toLowerCase().includes(keyword.trim().toLowerCase()) ||
+                    item.instructor.trim().toLowerCase().includes(keyword.trim().toLowerCase())
+            }).filter(item => { return item.category.trim().toLowerCase().includes(category.trim().toLowerCase()) })
         }
     }
 }
